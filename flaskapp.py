a68d32ef88019c93ee37c07863af78c3cf31e6e0
AkellerGit <abkgit@gmail.com>
2018-12-10 13:43:19 -0500

    added forms and BS styles

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config.update(dict(
+    SECRET_KEY="powerful secretkey",
+    WTF_CSRF_SECRET_KEY="a csrf secret key"
+))
 
 @app.route("/")
 @app.route("/home")
@@ -10,6 +16,16 @@ def home():
 @app.route("/about")
 def about():
 	return render_template('about.html')
+
+@app.route("/register")
+def register():
+	form = RegistrationForm()
+	return render_template('register.html', form=form)
+
+@app.route("/login")
+def login():
+	form = LoginForm()
+	return render_template('login.html', form=form)
 
 if __name__ == '__main__':
 	app.run(debug=True)
