a2c71ad98adf27c249c64314f2026a64d962f0bc
AkellerGit <abkgit@gmail.com>
2018-12-15 11:51:39 -0500

    added form verification and SQLAlchemy

--- flaskapp.py
+++ flaskapp.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config.update(dict(
     SECRET_KEY="powerful secretkey",
     WTF_CSRF_SECRET_KEY="a csrf secret key"
 ))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://site.db'
+
+db = SQLAlchemy(app)
+
+
 
 @app.route("/")
 @app.route("/home")
@@ -17,9 +23,13 @@ def home():
 def about():
 	return render_template('about.html')
 
-@app.route("/register")
+@app.route("/register", methods=['GET','POST'])
 def register():
 	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f'Account created for {form.username.data}!', 'Success')
+		return redirect(url_for('home'))
+
 	return render_template('register.html', form=form)
 
 @app.route("/login")
